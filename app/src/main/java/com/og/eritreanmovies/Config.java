0a46dd17bb790c759f9caef286d320940ed78e05
Robel OG <Robel.ghebrekiros@gmail.com>
2016-06-24 02:19:20 -0700

    Initial commit

---
+++ app/src/main/java/com/og/eritreanmovies/Config.java
@@ -0,0 +1,10 @@
+package com.og.eritreanmovies;
+
+/**
+ * Created by OG on 6/2/16.
+ */
+public class Config {
+    private Config(){}
+        public static final String YOUTUBE_API_KEY ="AIzaSyC4uDFry327ESxImRWmzZR-DkneYprzbNU";
+
+}
